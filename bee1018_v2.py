651830249130f2160eae022a34f67b5bdf1bf6d5
joaopedrotozettodacunha <joaopedrotozettodacunha@gmail.com>
2026-09-04 21:49:43 -0300

    Create bee1018_v2.py

---
+++ bee1018_v2.py
@@ -0,0 +1,11 @@
+valor = int(input())
+
+print(valor)
+
+notas = [100, 50, 20, 10, 5, 2, 1]
+
+for nota in notas:
+    quantidade = valor//nota
+    print(f"{quantidade} nota(s) de R$ {nota},00")
+
+    valor = valor % nota
